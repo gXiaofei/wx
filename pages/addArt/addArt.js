7fca14d6ab902c24ce4ff02615a8c28a835607d7
xiaofei[alias] <gongxiaofei@aqa.com>
2018-12-29 13:55:46 +0800

    -

--- pages/addArt/addArt.js
+++ pages/addArt/addArt.js
@@ -5,7 +5,12 @@ Page({
    * 页面的初始数据
    */
   data: {
-   
+    value: {
+      title: '',
+      content: ''
+    },
+    isSubmit: true,
+    isSave: true
   },
 
   /**
@@ -33,14 +38,44 @@ Page({
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+    const isSubmit = this.data.isSubmit;
+    const _this = this;
+    if (!isSubmit){
+      wx.showModal({
+        title: '是否要保存草稿？',
+        showCancel: true,
+        success: function (e) {
+          if (e.confirm){
+            _this.setData({
+              isSave: true,
+            })
+          }else{
+            _this.setData({
+              isSave: false,
+              value: {
+                title: '',
+                content: ''
+              }
+            })
+          }
+        }
+      })
+    }else{
+      this.setData({
+        isSave: false,
+        value: {
+          title: '',
+          content: ''
+        }
+      })
+    }
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+    console.log(33333);
   },
 
   /**
@@ -63,4 +98,68 @@ Page({
   onShareAppMessage: function () {
   
   },
+  // 保存标题
+  titleInput: function (e) {
+    const value = JSON.stringify(this.data.value);
+    this.setData({
+      isSubmit: false,
+      value: {
+        ...JSON.parse(value),
+        title: e.detail.value
+      }
+    })
+  },
+  // 保存内容
+  contentInput: function (e) {
+    const value = JSON.stringify(this.data.value);
+    this.setData({
+      isSubmit: false,
+      value: {
+        ...JSON.parse(value),
+        content: e.detail.value
+      }
+    })
+  },
+
+  // 提交
+  formSubmit: function (event) {
+    const value = this.data.value;
+    const userinfoStr = wx.getStorageSync('userinfo');
+    const uesrinfoObj = JSON.parse(userinfoStr);
+    
+    if (value.title.trim() !== '' && value.content.trim() !== ''){
+      wx.showLoading({
+        mask: true
+      });
+      this.setData({
+        isSubmit: true
+      });
+      wx.request({
+        url: 'http://localhost:9999/art/write',
+        method: 'POST',
+        data: {
+          ...value,
+          nickName: uesrinfoObj.data.nickName,
+          avatarUrl: uesrinfoObj.data.avatarUrl
+        },
+        success: function(result){
+          console.log('result', result);
+          if (result.statusCode === 200){
+            wx.hideLoading();
+            wx.showToast({
+              title: 'write success!\^o^/',
+              success: function () {
+                 // 跳转到详情     
+              }
+            });
+          }
+        }
+      })
+    }else{
+      wx.showToast({
+        title: '标题和内容都不能为空',
+        icon: 'none'
+      })
+    }
+  }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,61 +4,110 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    dataList: [],
+    pageNo: 1,
+    count: 1,
+    isLast: false,
+    type: 'index',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+    var pageNo = this.data.pageNo;
+    this.fetchList(pageNo);
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function(e) {
+    this.fetchList(1, false);
+    
+    this.setData({
+      pageNo: 1,
+      isLast: false,
+    })
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+    var pageNo = this.data.pageNo;
+    var count = this.data.count;
+    if (pageNo < count){
+      this.fetchList(pageNo + 1);
+      this.setData({
+        pageNo: pageNo + 1
+      })
+    }else{
+      this.setData({
+        isLast: true
+      })
+    }
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
+  },
+  // 请求列表
+  fetchList: function(pageNo ,push=true) {
+    var _this = this;
+    wx.showLoading({
+      mask: true
+    });
+    const data = this.data.dataList;
+    wx.request({
+      url: 'http://localhost:9999/art/info',
+      data: {
+        pageNo: pageNo,
+        pageSize: 9
+      },
+      method: 'GET',
+      success: function(result) {
+        console.log(result);
+        if (result.statusCode === 200) {
+          _this.setData({
+            dataList: push ? [...data, ...result.data.results] : result.data.results,
+            count: result.data.pageCount
+          })
+          wx.hideLoading();
+          !push && wx.stopPullDownRefresh();
+        }
+      }
+    })
   }
 })
